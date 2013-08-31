5c081fc840562525fec60bc54e510d6b95ed2ca6
anamaciel <anacm.maciel@gmail.com>
2013-08-31 17:58:34 -0300

    Implement social network month by month and using GraphToolkit

--- spa-sna/src/redestcc/GraphToolkitExample.java
+++ spa-sna/src/redestcc/GraphToolkitExample.java
@@ -11,9 +11,11 @@ import org.gephi.filters.api.Query;
 import org.gephi.filters.api.Range;
 import org.gephi.filters.plugin.graph.DegreeRangeBuilder.DegreeRangeFilter;
 import org.gephi.graph.api.DirectedGraph;
+import org.gephi.graph.api.Edge;
 import org.gephi.graph.api.GraphController;
 import org.gephi.graph.api.GraphModel;
 import org.gephi.graph.api.GraphView;
+import org.gephi.graph.api.Node;
 import org.gephi.io.exporter.api.ExportController;
 import org.gephi.io.importer.api.Container;
 import org.gephi.io.importer.api.EdgeDefault;
@@ -30,7 +32,6 @@ import org.gephi.project.api.Workspace;
 import org.gephi.ranking.api.Ranking;
 import org.gephi.ranking.api.RankingController;
 import org.gephi.ranking.api.Transformer;
-import org.gephi.ranking.plugin.transformer.AbstractColorTransformer;
 import org.gephi.ranking.plugin.transformer.AbstractSizeTransformer;
 import org.gephi.statistics.plugin.GraphDistance;
 import org.openide.util.Lookup;
@@ -102,11 +103,36 @@ public class GraphToolkitExample
 		distance.execute(graphModel, attributeModel);
 		 
 		//Rank color by Degree
-		Ranking degreeRanking = rankingController.getModel().getRanking(Ranking.NODE_ELEMENT, Ranking.DEGREE_RANKING);
+		/*Ranking degreeRanking = rankingController.getModel().getRanking(Ranking.NODE_ELEMENT, Ranking.DEGREE_RANKING);
 		AbstractColorTransformer colorTransformer = (AbstractColorTransformer) rankingController.getModel().getTransformer(Ranking.NODE_ELEMENT, Transformer.RENDERABLE_COLOR);
 		colorTransformer.setColors(new Color[]{new Color(0xFEF0D9), new Color(0xB30000)});
-		rankingController.transform(degreeRanking,colorTransformer);
-		 
+		rankingController.transform(degreeRanking,colorTransformer);*/
+		
+		//List node columns
+		AttributeController ac = Lookup.getDefault().lookup(AttributeController.class);
+		AttributeModel model2 = ac.getModel();
+		for (AttributeColumn col : model2.getNodeTable().getColumns()) {
+		    System.out.println(col);
+		}
+		
+		AttributeColumn sourceCol = model2.getNodeTable().getColumn("newcomer");
+		//System.out.println("teste: "+ sourceCol.toString());
+		for (Node n : graphModel.getGraph().getNodes()) {
+		    //System.out.println(n.getNodeData().getAttributes().getValue("newcomer"));
+			//n.getNodeData().setLabel((String)n.getNodeData().getAttributes().getValue("Id"));
+		    if(n.getNodeData().getAttributes().getValue("newcomer").equals("true")){
+		    	n.getNodeData().setColor(1f, 0.6f, 1f);
+		    	n.getNodeData().setLabel((String)n.getNodeData().getAttributes().getValue("Id"));
+		    }else{
+		    	n.getNodeData().setLabel("");
+		    }
+		}
+		
+		/*for (Edge e : graphModel.getGraph().getEdges()) {
+			e.getEdgeData().setLabel(e.getSource().getNodeData().getId() + " - " + e.getTarget().getNodeData().getId());
+		}*/
+		
+				 
 		//Rank size by centrality
 		AttributeColumn centralityColumn = attributeModel.getNodeTable().getColumn(GraphDistance.BETWEENNESS);
 		Ranking centralityRanking = rankingController.getModel().getRanking(Ranking.NODE_ELEMENT, centralityColumn.getId());
@@ -117,15 +143,16 @@ public class GraphToolkitExample
 		 
 		//Preview
 		model.getProperties().putValue(PreviewProperty.EDGE_CURVED, Boolean.FALSE);
-		model.getProperties().putValue(PreviewProperty.SHOW_NODE_LABELS, Boolean.FALSE);
+		model.getProperties().putValue(PreviewProperty.SHOW_NODE_LABELS, Boolean.TRUE);
 		model.getProperties().putValue(PreviewProperty.EDGE_COLOR, new EdgeColor(Color.GRAY));
+		model.getProperties().putValue(PreviewProperty.SHOW_EDGE_LABELS, Boolean.TRUE);
 		model.getProperties().putValue(PreviewProperty.EDGE_THICKNESS, new Float(0.1f));
 		model.getProperties().putValue(PreviewProperty.NODE_LABEL_FONT, model.getProperties().getFontValue(PreviewProperty.NODE_LABEL_FONT).deriveFont(8));
 		 
 		//Export
 		ExportController ec = Lookup.getDefault().lookup(ExportController.class);
 		try {
-		    ec.exportFile(new File("/home/magsilva/headless_simple.pdf"));
+		    ec.exportFile(new File("C:/Users/AnaaMaciel/Documents/UTFPR/TCC/REDESMES/GraphToolkit/Semestre/2012-02Label2.pdf"));
 		} catch (Exception e) {
 		    throw new IllegalArgumentException(e);
 		}
@@ -133,6 +160,6 @@ public class GraphToolkitExample
 	
 	public static void main(String[] args)
 	{
-		GraphToolkitExample gte = new GraphToolkitExample(new File("/home/magsilva/redeOldsNewsCompletav2.gml"));
+		GraphToolkitExample gte = new GraphToolkitExample(new File("C:/Users/AnaaMaciel/Documents/UTFPR/TCC/REDESMES/Semestre/2012-02.gml"));
 	}
 }
